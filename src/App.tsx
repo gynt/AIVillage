209a31fd7965c6d07767c0d0289d6fb6c6e9a397
Gynt <Gynt@users.noreply.github.com>
2024-08-25 20:13:38 +0200

    rendering method 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
+import { useCallback, useMemo, useRef, useState } from 'react'
+
+import { atom, getDefaultStore, useAtom, useAtomValue } from 'jotai';
+import { atomFamily } from 'jotai/utils';
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const randomColor = () => `#${Math.floor(Math.random() * 255).toString(16)}${Math.floor(Math.random() * 255).toString(16)}${Math.floor(Math.random() * 255).toString(16)}`;
+const highlightAtom = atomFamily((id) => atom(randomColor()));
+type Position = {x: number; y:number }
+const positionAtom = atom<Position>()
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const selectedObjectAtom = atom<number>(5);
+
+type GridElementProps = {
+  id: number;
+}
+const GridElement = (props: GridElementProps) => {
+  const [color, setColor] = useAtom(highlightAtom(props.id))
+  return (<div style={{
+    backgroundColor: color,
+    width: '8px',
+    height: '8px',
+  }}
+  onMouseEnter={() => {
+    // setColor(randomColor())
+    const pos = {x: props.id % 100, y: ~~(props.id / 100)};
+    getDefaultStore().set(positionAtom, pos)
+    for(const i of [...Array(getDefaultStore().get(selectedObjectAtom)).keys()]) {
+      for (const j of [...Array(getDefaultStore().get(selectedObjectAtom)).keys()]) {
+        getDefaultStore().set(highlightAtom( (pos.x + i) + ((pos.y + j)*100)), 'green');
+      }
+    }
+  }}
+  ></div>)
 }
 
-export default App
+function App() {
+  const divRef = useRef(null);
+
+  const position = useAtomValue(positionAtom) || {x: -1, y:-1};
+  const {x, y} = position;
+  const objectSize = useAtomValue(selectedObjectAtom)
+
+  useCallback(() => {
+    for(const i of [...Array(objectSize).keys()]) {
+      for (const j of [...Array(objectSize).keys()]) {
+        getDefaultStore().set(highlightAtom( i + (j*100)), 'green');
+      }
+    }
+  }, [x, y])
+
+  const grid = useMemo(() => {
+    return (<div ref={divRef} style={{
+      width: '800px',
+      height: '800px',
+      backgroundColor: 'black',
+      display: 'grid',
+      gridTemplateColumns: 'repeat(100, 1fr)',
+      padding: '0',
+
+    }}
+    >
+      {Array.from(new Array(100*100).keys()).map((i) => <GridElement id={i}/>)}
+    </div>)
+  }, [])
+
+  return (
+    <div className="App">
+      {grid}
+      <div>{x}, {y}</div>
+    </div>
+    
+  );
+}
+
+export default App;
